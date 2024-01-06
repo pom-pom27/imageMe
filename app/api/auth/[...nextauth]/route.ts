fb01e9b6e38401854639df2b83127e3997087367
selja <seljaworks@gmail.com>
2024-01-06 17:31:04 +0700

    add nextauth secret property

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,6 +8,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async session({ session }) {
       return session;
